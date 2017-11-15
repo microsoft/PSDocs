6a3c36e22b372521bafcbc2ae9b76ed0e27d6796
Bernie White <bewhite@microsoft.com>
2017-11-15 18:11:22 +1000

    Update documentation build tasks

--- scripts/build-docs.ps1
+++ scripts/build-docs.ps1
@@ -1,15 +1,16 @@
 
 [CmdletBinding()]
 param (
-    [Switch]$Scaffold = $False`
+    [Switch]$Scaffold = $False
 )
 
-Import-Module '.\src\PSDocs' -Force;
-Import-Module '.\src\PSDocs.Dsc' -Force;
-
-
 if ($Scaffold) {
+    Import-Module '.\src\PSDocs' -Force;
+    Import-Module '.\src\PSDocs.Dsc' -Force;
 
     Update-MarkdownHelp -Path '.\docs\commands\PSDocs\en-US';
     Update-MarkdownHelp -Path '.\docs\commands\PSDocs.Dsc\en-US';
 }
+
+New-ExternalHelp -OutputPath '.\build\docs' -Path '.\docs\commands\PSDocs\en-US' -Force;
+New-ExternalHelp -OutputPath '.\build\docs' -Path '.\docs\commands\PSDocs.Dsc\en-US' -Force;
