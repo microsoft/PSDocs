b854f6ba7509cf5013d7b68bbc9b63f5f6d1dd95
Bernie White <bewhite@microsoft.com>
2017-12-01 07:55:45 +1000

    Updated script to clean up old docs

--- scripts/build-docs.ps1
+++ scripts/build-docs.ps1
@@ -14,9 +14,17 @@ if ($Scaffold) {
     return;
 }
 
+# Generate external help files
 New-ExternalHelp -OutputPath '.\build\docs\PSDocs' -Path '.\docs\commands\PSDocs\en-US','.\docs\keywords\PSDocs\en-US' -Force;
 New-ExternalHelp -OutputPath '.\build\docs\PSDocs.Dsc' -Path '.\docs\commands\PSDocs.Dsc\en-US' -Force;
 
+# Clean up old external help files
+Remove-Item -Path '.\src\PSDocs\**\*.help.txt';
+Remove-Item -Path '.\src\PSDocs\**\*-help.xml';
+Remove-Item -Path '.\src\PSDocs.Dsc\**\*.help.txt';
+Remove-Item -Path '.\src\PSDocs.Dsc\**\*-help.xml';
+
+# Coyp new external help files
 Copy-Item -Path '.\build\docs\PSDocs\*.*' -Destination '.\src\PSDocs\en-US';
 Copy-Item -Path '.\build\docs\PSDocs\*.*' -Destination '.\src\PSDocs\en-AU';
 Copy-Item -Path '.\build\docs\PSDocs.Dsc\*.*' -Destination '.\src\PSDocs.Dsc\en-US';
