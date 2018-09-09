8bbec6e5e98bb57982c08d278a7c44d98b48a568
Bernie White <bewhite@microsoft.com>
2018-09-09 18:45:14 +1000

    Added text node

---
+++ src/PSDocs/Models/Text.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PSDocs.Models
+{
+    public sealed class Text : DocumentNode
+    {
+        public override DocumentNodeType Type => DocumentNodeType.Text;
+
+        public string Content { get; set; }
+    }
+}
