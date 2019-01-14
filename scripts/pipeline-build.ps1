9a1c9d1e624b1bd3dac906f2b8e8ccc21297fd7d
Bernie White <bewhite@microsoft.com>
2019-01-14 18:25:46 +1000

    Updated minimum PowerShellGet version to 2.0.3

--- scripts/pipeline-build.ps1
+++ scripts/pipeline-build.ps1
@@ -37,6 +37,12 @@ if ($Null -eq (Get-PackageProvider -Name NuGet -ErrorAction SilentlyContinue)) {
     Install-PackageProvider -Name NuGet -Force -Scope CurrentUser;
 }
 
+if ($Null -eq (Get-InstalledModule -Name PowerShellGet -MinimumVersion 2.0.3 -ErrorAction SilentlyContinue)) {
+    Install-Module PowerShellGet -MinimumVersion 2.0.3 -Scope CurrentUser -Force;
+    Remove-Module -Name PowerShellGet -ErrorAction SilentlyContinue;
+    Import-Module -Name PowerShellGet -MinimumVersion 2.0.3;
+}
+
 if ($Null -eq (Get-Module -Name InvokeBuild -ListAvailable -ErrorAction SilentlyContinue | Where-Object -FilterScript { $_.Version -like '5.*' })) {
     Install-Module InvokeBuild -MinimumVersion 5.4.0 -Scope CurrentUser -Force;
 }
